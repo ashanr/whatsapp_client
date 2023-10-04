e3f787c5b1b71883ffceb4bd4997710fece736d5
Ashan Rajapaksha <rajapaksha.live@gmail.com>
2023-10-04 07:24:03 +0530

    Login Component added to fetch QR code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LoginComponent msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginComponent from './components/LoginComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginComponent
   }
 }
 </script>
---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <h1>{{ msg }}</h1>
+      <div v-if="qrCode">
+        <img :src="qrCode" alt="WhatsApp QR Code" />
+      </div>
+      <button @click="fetchQRCode">Fetch QR Code</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        qrCode: null,
+      };
+    },
+    methods: {
+      async fetchQRCode() {
+        // Fetch QR code from the backend
+        const response = await fetch('http://localhost:3000/get-qr-code');
+        const data = await response.json();
+        this.qrCode = data.qrImageUrl;
+      },
+    },
+  };
+  </script>
+  
